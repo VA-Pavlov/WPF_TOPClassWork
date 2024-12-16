00be41118d475396a28cba9c12d552c3ec0af4c5
VA-Pavlov <vap.uspeh@gmail.com>
2024-12-16 20:16:10 +0300

    Задача выполнена

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,16 @@ namespace WPF_TOPClassWork
         public MainWindow()
         {
             InitializeComponent();
+            Binding binding = new Binding();
+
+            binding.ElementName = "SylfaenBox"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            SylfaenBlock.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
+
+            binding = new Binding();
+            binding.ElementName = "CalibriBox"; // элемент-источник
+            binding.Path = new PropertyPath("Text"); // свойство элемента-источника
+            CalibriBlock.SetBinding(TextBlock.TextProperty, binding); // установка привязки для элемента-приемника
         }
     }
 }
