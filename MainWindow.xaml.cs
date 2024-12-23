f3420b921d2a785923f48fa20df7cd94b0f2000e
VA-Pavlov <vap.uspeh@gmail.com>
2024-12-23 15:01:00 +0300

    Шаблон прилоэения без дизайна

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -28,22 +28,25 @@ namespace WPF_TOPClassWork
 
             makes = new ObservableCollection<Make>()
             {
-                new Make{Date=DateTime.Now,Title="iPhone 6S", Description="qwweq wqe qweqwe qw ew sdasdw  dasdwe dasdf ersd d f asada feddsg ffsddfda asdas faasdd as"}
+                new Make{Date=DateTime.Now,Title="Дело 1", Description="qwweq wqe qweqwe qw ew sdasdw  dasdwe dasdf ersd d f asada feddsg ffsddfda asdas faasdd as"}
             };
 
-
-            phonesList.ItemsSource = makes;
+            //maksList.ItemsSource = makes;
         }
 
-        //private void Add_Click(object sender, RoutedEventArgs e)
-        ////{
-        ////    var phone = new Phone() { Id = Int32.Parse(IdBox.Text), Title = TitleBox.Text, Company = CompanyBox.Text };
-        ////    phones.Add(phone);
-        //}
-        //private void Delete_Click(object sender, RoutedEventArgs e)
-        //{
-        //    //phones.Remove((Phone)phonesList.SelectedItem);
-        //}
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            var make = new Make {
+                Date = DateBox.DisplayDate,
+                Title = TitleBox.Text,
+                Description = DescriptionBox.Text
+            };
+            makes.Add(make);
+        }
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            //makes.Remove((Make)maksList.SelectedItem);
+        }
 
         //private void phonesList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         //{
