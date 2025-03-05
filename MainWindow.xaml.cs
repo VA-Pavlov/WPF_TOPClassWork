20e9aa979713565a4f5763e7178b8dae435c9812
pavlov-va <vap.uspeh@gmail.com>
2025-03-05 17:27:30 +0300

    Реализовано тренировочное задание перед дз

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,91 @@ namespace WPF_TOPClassWork
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Dictionary<string, MyButton> ButtonsDictionary = new Dictionary<string, MyButton>();
         public MainWindow()
         {
             InitializeComponent();
+            CreateDictionary();
+        }
+
+        private void CreateDictionary()
+        {
+            foreach (var line in MainStack.Children)
+            {
+                if (line is StackPanel)
+                    foreach (var border in (line as StackPanel).Children)
+                    {
+                        var myButton = new MyButton(border as Border);
+                        if (char.IsLower(myButton.SmalValue[0]) && myButton.SmalValue.Length == 1)
+                        {
+                            ButtonsDictionary.Add(myButton.BigValue, myButton);
+                        }
+                        else if (char.IsDigit(myButton.SmalValue[0]))
+                            ButtonsDictionary.Add("D" + myButton.SmalValue, myButton);
+                        else
+                        {
+                            ButtonsDictionary.Add("LeftShift", myButton);
+                        }
+                    }
+                else
+                    ButtonsDictionary.Add("Space", (new MyButton((line as Border))));
+            }
+        }
+
+        private void TextBlock_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (ButtonsDictionary.TryGetValue(e.Key.ToString(), out var button))
+            {
+                switch (e.Key)
+                {
+                    case Key.Space:
+                        Text.Text += " ";
+                        break;
+                    case Key.LeftShift:
+                        ButtonUp();
+                        break;
+                    default:
+                        Text.Text += button.TextBlock.Text;
+                        break;
+                }
+                button.Border.Background = Brushes.LightGray;
+            }
+        }
+        private void ButtonUp()
+        {
+            foreach (var key in ButtonsDictionary.Keys)
+                ButtonsDictionary[key].TextBlock.Text = ButtonsDictionary[key].BigValue;
+
+        }
+
+        private void Text_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (ButtonsDictionary.TryGetValue(e.Key.ToString(), out var button))
+            {
+                button.Border.Background = new SolidColorBrush(button.Color);
+                if (e.Key == Key.LeftShift)
+                    ButtonDown();
+            }
+        }
+        private void ButtonDown()
+        {
+            foreach (var key in ButtonsDictionary.Keys)
+                ButtonsDictionary[key].TextBlock.Text = ButtonsDictionary[key].SmalValue;
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+            Text.Focus();
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+            Keyboard.ClearFocus();
         }
     }
 }
---
+++ MyButton.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WPF_TOPClassWork
+{
+    public class MyButton
+    {
+        public string SmalValue { get; set; }
+        public string BigValue { get; set; }
+        public Color Color { get; set; }
+        public TextBlock TextBlock { get; set; }
+        public Border Border { get; set; }
+        public MyButton(Border border)
+        {
+            TextBlock = border.Child as TextBlock;
+            Border = border;
+            Color = (border.Background as SolidColorBrush).Color;
+            if (!char.IsDigit(TextBlock.Text[0]) && TextBlock.Text.Length == 1)
+            {
+                SmalValue = TextBlock.Text;
+                BigValue = SmalValue.ToUpper();
+            }
+            else
+            {
+                SmalValue = TextBlock.Text;
+                switch (SmalValue)
+                {
+                    case "4":
+                        BigValue = "$";
+                        break;
+                    case "5":
+                        BigValue = "%";
+                        break;
+                    case "6":
+                        BigValue = "^";
+                        break;
+                    case "7":
+                        BigValue = "&";
+                        break;
+                    default:
+                        BigValue = SmalValue;
+                        break;
+                }
+            }
+        }
+    }
+}
